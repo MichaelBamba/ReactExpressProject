020c2f40adc1b2fc2e58f3d7c443f89205ab2206
MichaelBamba <mjbamba2@gmail.com>
2020-04-03 15:21:24 -0400

    reworked blogpost to do the api pull need to keep reworking it

--- src/components/blogpost.jsx
+++ src/components/blogpost.jsx
@@ -1,14 +1,33 @@
 import React, { Component } from "react";
 
-const BlogPost = (props) => {
-  const { blogposts } = props;
+class BlogPost extends Component {
+  state = {
+    post: {},
+  };
 
-  return (
-    <div>
-      <p>{blogposts.author}</p>
-      <p> {blogposts.post} </p>
-    </div>
-  );
-};
+  async componentDidMount() {
+    const post = await this.loadData();
+    this.setState({
+      post,
+    });
+  }
+
+  loadData = async () => {
+    const url = `http://localhost:3000/post`;
+    const response = await fetch(url);
+    const data = response.json();
+    return data;
+  };
+
+  render() {
+    const { blogposts } = this.state;
+    return (
+      <div>
+        <h2>{blogposts.author}</h2>
+        <p>{blogposts.content}</p>
+      </div>
+    );
+  }
+}
 
 export default BlogPost;
